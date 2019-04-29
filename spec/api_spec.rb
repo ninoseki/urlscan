11a212345d3ab53a21a15093aa153dfa9ffbf1dd
Manabu Niseki <manabu.niseki@gmail.com>
2019-04-29 23:17:11 +0900

    refactor: refactoring specs

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,22 +1,23 @@
 RSpec.describe UrlScan::API, :vcr do
-  context "#submit" do
-    it "should return a response" do
-      json = subject.submit("https://www.wikipedia.org/", true)
-      expect(json).to be_a(Hash)
+  let(:api) { UrlScan::API.new }
+
+  describe "#submit" do
+    it do
+      json = api.submit("https://www.wikipedia.org/", true)
       expect(json["message"]).to eq("Submission successful")
     end
   end
 
-  context "#result" do
-    it "should return a response" do
-      json = subject.result("7f0aa2ab-748a-4cae-b648-71e324e836cd")
+  describe "#result" do
+    it do
+      json = api.result("7f0aa2ab-748a-4cae-b648-71e324e836cd")
       expect(json).to be_a(Hash)
     end
   end
 
-  context "#search" do
-    it "should return a response" do
-      json = subject.search("domain:wikipedia.org")
+  describe "#search" do
+    it do
+      json = api.search("domain:wikipedia.org")
       expect(json).to be_a(Hash)
     end
   end
